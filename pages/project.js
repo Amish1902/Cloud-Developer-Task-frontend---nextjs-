f95e5443990b80630641e4e1a1187a0c658ca0be
Amish1902 <72759107+Amish1902@users.noreply.github.com>
2022-07-22 01:52:43 +0530

    Create project.js

---
+++ pages/project.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const Project = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello World my Project</h1>
+    </>
+  );
+};
+
+export default Project;
